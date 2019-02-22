bf10ea0380381459fddafe7b11487488bed14425
jtingey <j.tingey.16@ucl.ac.uk>
2019-02-22 13:17:17 +0000

    Updated how the DataHandler stores the data and then normalises it if required, also made the beamE the first index in parameters

--- cvn/network.py
+++ cvn/network.py
@@ -1,6 +1,7 @@
 # The implementation of the PID and Parameter Estimation Convolutional Visual Networks
 import os
 import talos as ta
+import argparse
 
 import models
 import utils
@@ -33,32 +34,27 @@ class PIDNetwork(Network): # Particle Identification (PID) Network Class
 		model = models.pid_model(categories, self.data.get_image_shape(), self.learning_rate)	
 
 		# Get the images from the different samples
-		train_data = self.data.train_data["images_norm"]
-		val_data = self.data.val_data["images_norm"]
-		test_data = self.data.test_data["images_norm"]
-		if not self.norm:
-			train_data = self.data.train_data["images"]
-			val_data = self.data.val_data["images"]
-			test_data = self.data.test_data["images"]			
+		train_images, val_images, test_images = self.data.get_images(self.norm)		
 
 		# Fit the model with the training data and store the training "history"
-		train_history = model.fit(train_data, self.data.train_data["labels"],
+		train_history = model.fit(train_images, self.data.data["train_labels"],
 								  batch_size=self.batch_size, epochs=self.num_epochs, verbose=1,
-								  validation_data=(val_data, self.data.val_data["labels"]),
+								  validation_data=(val_images, self.data.data["val_labels"]),
 								  callbacks=[utils.callback_checkpoint(self.weights),
 								  			 utils.callback_early_stop("val_acc", 0.01, 5)])    
 
 		# Score the model
-		score = model.evaluate(test_data, self.data.test_data["labels"], verbose=0)
+		score = model.evaluate(test_images, self.data.data["test_labels"], verbose=0)
 		print('Test Loss: {:.4f}, Test Accuracy: {:.4f}'.format(score[0], score[1]))	
 
 		# Save history to training output
 		utils.save_category_history(train_history, self.output_name)	
 
 		# Evaluate the test sample on the trained model and save output to file
-		test_output = model.predict(test_data, verbose=0)
-		utils.save_category_output(categories, test_labels, test_energies, 
-								   test_parameters, test_output, self.output_name)
+		test_output = model.predict(test_images, verbose=0)
+		utils.save_category_output(categories, self.data.data["test_labels"], 
+								   self.data.data["test_pars"], 
+								   test_output, self.output_name)
 
 	def evaluate(self):
 		print("PIDNetwork: evaluate() ...")
@@ -66,11 +62,8 @@ class PIDNetwork(Network): # Particle Identification (PID) Network Class
 	def optimise(self):
 		print("PIDNetwork: optimise() ...")
 
-		x = self.data.train_data["images_norm"]
-		y = self.data.train_data["labels"]
-
-		if not self.norm:
-			x = self.data.train_data["images"]
+		x, val_images, test_images = self.data.get_images(self.norm)		
+		y = self.data.data["train_labels"]		
 
 		p = {"learning_rate":[0.001], "batch_size":[500], "epochs":[40],
 			 "filters_1":[64], "size_1":[3], "pool_1":[2],
@@ -97,16 +90,10 @@ class PPENetwork(Network): # Particle Parameter Estimation (PPE) Network Class
 		else:
 			name, ext = os.path.splitext(self.output_name)			
 			weight_name, weight_ext = os.path.splitext(self.weights)		
-			for par in range(7):
+			for par in range(8):
 				# Change the output file name everytime
 				self.output_name = name + "_" + str(par) + ".txt"	
 				self.weights = weight_name + "_" + str(par) + ".ckpt"
-
-				# Don't use the normalised images for vtxT and track energy
-				if par == 3 or par == 6:
-					self.norm = False	
-				else:
-					self.norm = True
 
 				# Set the parameter to train
 				self.parameter = par
@@ -121,35 +108,31 @@ class PPENetwork(Network): # Particle Parameter Estimation (PPE) Network Class
 
 		# Get the specific Keras parameter model
 		model = models.parameter_model(self.parameter, self.data.get_image_shape(), 
-										   self.learning_rate)
+									   self.learning_rate)
 
 		# Get the images from the different samples
-		train_data = self.data.train_data["images_norm"]
-		val_data = self.data.val_data["images_norm"]
-		test_data = self.data.test_data["images_norm"]
-		if not self.norm:
-			train_data = self.data.train_data["images"]
-			val_data = self.data.val_data["images"]
-			test_data = self.data.test_data["images"]			
+		train_images, val_images, test_images = self.data.get_images(self.norm)				
 
 		# Fit the model with the training data and store the training "history"
-		train_history = model.fit(train_data, self.data.train_data["parameters"][:, self.parameter],
+		train_history = model.fit(train_images, self.data.data["train_pars"][:, self.parameter],
 								  batch_size=self.batch_size, epochs=self.num_epochs, verbose=1,
-								  validation_data=(val_data, self.data.val_data["parameters"][:, self.parameter]),
-								  callbacks=[utils.callback_checkpoint(self.weights),
-								  			 utils.callback_early_stop("val_mean_absolute_error", 5, 5)])
+								  validation_data=(val_images, self.data.data["val_pars"][:, self.parameter]),
+								  callbacks=[])
+
+		# utils.callback_checkpoint(self.weights)
+		# utils.callback_early_stop("val_mean_absolute_error", 5, 5)
 		
 		trained_epochs = len(train_history.history['loss'])
-		print(trained_epochs)
 
 		if self.output_name is not None:
 			# Save history to training output
 			utils.save_regression_history(train_history, self.output_name)
 
 			# Evaluate the test sample on the trained model and save output to file
-			test_output = model.predict(test_data, verbose=0)
-			utils.save_regression_output(self.data.test_data["labels"], self.data.test_data["energies"], 
-										 self.data.test_data["parameters"], test_output, self.output_name)
+			test_output = model.predict(test_images, verbose=0)
+			utils.save_regression_output(self.data.data["test_labels"], 
+										 self.data.data["test_pars"], 
+										 test_output, self.output_name)
 
 		return train_history.history['val_mean_absolute_error'][trained_epochs-1]        
 
@@ -159,10 +142,8 @@ class PPENetwork(Network): # Particle Parameter Estimation (PPE) Network Class
 	def optimise(self):
 		print("PPENetwork: optimise() ...")
 
-		x = self.data.train_data["images_norm"], self.data.val_data["images_norm"]
-		y = self.data.train_data["parameters"][:, self.parameter]
-		if not self.norm:
-			x = self.data.train_data["images"]
+		x, val_images, test_images = self.data.get_images(self.norm)		
+		y = self.data.data["train_labels"]	
 
 		p = {"learning_rate":[0.001], "batch_size":[500], "epochs":[40],
 			 "filters_1":[32], "size_1":[3], "pool_1":[2],
@@ -173,8 +154,61 @@ class PPENetwork(Network): # Particle Parameter Estimation (PPE) Network Class
 					params=p, dataset_name='parameter_model', experiment_no='0',
 					clear_tf_session=False, val_split=0.2)
 
+# Parse the command line argument options
+def parse_args():
+	parser = argparse.ArgumentParser(description='Train/Evaluate/Optimise CHIPS CVN Networks...')
+
+	# Input and output files (history file will have similar name to output file)
+	parser.add_argument('input', help = 'Path to combined input "image" .txt file')
+	parser.add_argument('-o', '--output',    default='../../output/output.txt', help = 'Output .txt file')
+	parser.add_argument('-n', '--network',	 default = 'ppe', 	help = 'Type of network pid or ppe')
+
+	# What function are we doing?
+	parser.add_argument('--train', 		action = 'store_true',  help = 'Use to train the network')
+	parser.add_argument('--eval',  		action = 'store_true',  help = 'Use to evaluate on the network')
+	parser.add_argument('--opt', 	action = 'store_true', 		help = 'Use to optimise the network')
+
+	# Network Parameters
+	parser.add_argument('-p', '--parameter',	default = -1,	help = 'Parameter to fit (lepton Energy = 6)')
+	parser.add_argument('-w', '--weights', 	 	default='../../output/cp.ckpt',    help = 'Network weights')
+	parser.add_argument('-v', '--val_frac',   	default = 0.1,  help = 'Fraction of events for validation (0.1)')
+	parser.add_argument('-t', '--test_frac',  	default = 0.1,  help = 'Fraction of events for testing (0.1)')
+	parser.add_argument('-b', '--batch_size', 	default = 500,  help = 'Training batch size (500)')
+	parser.add_argument('-l', '--l_rate',     	default = 0.001,help = 'Training learning rate (0.001)')
+	parser.add_argument('-e', '--epochs',    	default = 20,   help = 'Training epochs (10)')
+	parser.add_argument('--no_hit',  	action = 'store_true', 	help = 'Do not use hit channel')
+	parser.add_argument('--no_time', 	action = 'store_true', 	help = 'Do not use time channel')
+	parser.add_argument('--norm',   		default = 'none',	help = '(none, ebe, sss)')
+	parser.add_argument('-s', '--image_size', 	default = 32, 	help = 'Input image size (32)')
+
+	# Check arguments
+	args = parser.parse_args()
+
+	if args.network != "ppe" and args.network != "pid":
+		print("Error: Specify Network!")
+		sys.exit()			
+	elif args.no_hit and args.no_time:
+		print("Error: Need to use at least one channel!")
+		sys.exit()	
+	elif args.train and args.eval or args.train and args.opt or args.eval and args.opt:
+		print("Error: Can only do one thing at a time!")
+		sys.exit()
+	elif args.train and args.eval and args.opt:
+		print("Error: Can only do one thing at a time!")
+		sys.exit()	
+	elif not args.train and not args.eval and not args.opt:
+		print("Error: Need to specify something to do!")
+		sys.exit()	
+
+	if args.norm not in ["none", "ebe", "sss"]:
+		print("Error: Norm needs to be [none, ebe, sss]")
+		sys.exit()					
+
+	return args  
+
+
 def main():
-	args = utils.parse_args() # Get the command line arguments
+	args = parse_args() # Get the command line arguments
 
 	# Load the data into the DataHandler and print the summary
 	data = utils.DataHandler(args.input, float(args.val_frac), float(args.test_frac),
--- cvn/utils.py
+++ cvn/utils.py
@@ -2,62 +2,9 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras import callbacks
 import matplotlib.pyplot as plt
-import os
 import sys
-import argparse
+import os
 from sklearn.preprocessing import StandardScaler
-
-###################################################
-#            Network Argument Handling            #
-###################################################
-
-# Parse the command line argument options
-def parse_args():
-	parser = argparse.ArgumentParser(description='Train/Evaluate/Optimise CHIPS CVN Networks...')
-
-	# Input and output files (history file will have similar name to output file)
-	parser.add_argument('input', help = 'Path to combined input "image" .txt file')
-	parser.add_argument('-o', '--output',    default='../../output/output.txt', help = 'Output .txt file')
-	parser.add_argument('-n', '--network',	 default = 'ppe', 	help = 'Type of network pid or ppe')
-	parser.add_argument('-w', '--weights', 	 default='../../output/cp.ckpt',    help = 'Network weights')
-
-	# What function are we doing?
-	parser.add_argument('--train', 		action = 'store_true',  help = 'Use to train the network')
-	parser.add_argument('--eval',  		action = 'store_true',  help = 'Use to evaluate on the network')
-	parser.add_argument('--opt', 	action = 'store_true', 		help = 'Use to optimise the network')
-
-	# Network Hyperparameters
-	parser.add_argument('-p', '--parameter',	default = -1,	help = 'Parameter to fit (lepton Energy = 6)')
-	parser.add_argument('-v', '--val_frac',   	default = 0.1,  help = 'Fraction of events for validation (0.1)')
-	parser.add_argument('-t', '--test_frac',  	default = 0.1,  help = 'Fraction of events for testing (0.1)')
-	parser.add_argument('-b', '--batch_size', 	default = 500,  help = 'Training batch size (500)')
-	parser.add_argument('-l', '--l_rate',     	default = 0.001,help = 'Training learning rate (0.001)')
-	parser.add_argument('-e', '--epochs',    	default = 10,   help = 'Training epochs (10)')
-	parser.add_argument('--no_hit',  	action = 'store_true', 	help = 'Do not use hit channel')
-	parser.add_argument('--no_time', 	action = 'store_true', 	help = 'Do not use time channel')
-	parser.add_argument('--norm',   	action = 'store_true',	help = 'Normalise the channels')
-	parser.add_argument('-s', '--image_size', 	default = 32, 	help = 'Input image size (32)')
-
-	# Check arguments
-	args = parser.parse_args()
-
-	if args.network != "ppe" and args.network != "pid":
-		print("Error: Specify Network!")
-		sys.exit()			
-	elif args.no_hit and args.no_time:
-		print("Error: Need to use at least one channel!")
-		sys.exit()	
-	elif args.train and args.eval or args.train and args.opt or args.eval and args.opt:
-		print("Error: Can only do one thing at a time!")
-		sys.exit()
-	elif args.train and args.eval and args.opt:
-		print("Error: Can only do one thing at a time!")
-		sys.exit()	
-	elif not args.train and not args.eval and not args.opt:
-		print("Error: Need to specify something to do!")
-		sys.exit()			
-
-	return args  
 
 ###################################################
 #                   DataHandler                   #
@@ -72,15 +19,10 @@ class DataHandler:
 		self.image_size = image_size	# Image size (32, 64)
 		self.no_hit = no_hit			# Don't use hit channel (bool)
 		self.no_time = no_time			# Don't use time channel (bool)
+		self.data = None				# Data dictionary
+		self.scaler = None				# StandardScaler for both channels (hit,time)
 
-		# Data is filled into these three sets in order
-		self.train_data = None			# Training data dictionary
-		self.val_data = None			# Validation data dictionary
-		self.test_data = None			# Testing data dictionary
-
-		self.hit_scaler = None			# Hit channel StandardScaler()
-		self.time_scaler = None			# Time channel StandardScaler()
-
+	# Loads the data from .txt file splitting up the arrays and places them in a large dictionary
 	def load_data(self):
 		print("DataHandler: load_data() ...")
 
@@ -96,90 +38,83 @@ class DataHandler:
 		# Split into training, validation and test samples
 		split_data = np.split(data, [num_training, num_training+num_validate, total_events])		
 
-		# Return the samples separately (Training, Validation, Testing)
-		self.data_to_dict(split_data[0])
-		self.data_to_dict(split_data[1])
-		self.data_to_dict(split_data[2])	
+		train_labels, train_pars, train_hit_images, train_time_images = self.decode(split_data[0])
+		val_labels, val_pars, val_hit_images, val_time_images = self.decode(split_data[1])
+		test_labels, test_pars, test_hit_images, test_time_images = self.decode(split_data[2])
 
-	# Extract the .txt file data formatting splitting into different numpy arrays and normalise the images
-	# [Labels(1), Beam Energies(1), Lepton Parameters(7), charge_pixels(32*32), time_pixels(32*32)]
-	def data_to_dict(self, data):
-		# Size of the image
-		image_data_length = self.image_size * self.image_size
+		# Set the data dictionary
+		self.data = {"train_labels":train_labels, "train_pars":train_pars,
+					 "train_hit_images":train_hit_images, "train_time_images":train_time_images,
+					 "val_labels":val_labels, "val_pars":val_pars,
+					 "val_hit_images":val_hit_images, "val_time_images":val_time_images,
+					 "test_labels":test_labels, "test_pars":test_pars,
+					 "test_hit_images":test_hit_images, "test_time_images":test_time_images}
 
-		# Split the data into the different numpy arrays we need
-		labels      = data[:, 0]        				# Index 0
-		energies    = data[:, 1]        				# Index 1
-		parameters  = data[:, 2:9]      				# Index 2-8
-		hit_images  = data[:, 9:image_data_length+9]  	# Index 9-1032
-		time_images = data[:, image_data_length+9:]   	# Index 1033-2057
+		# Fit the Standard Scalers for the two channels just on the training data
+		# We do not want the val/test sets to influence the model
+		hit_scaler = StandardScaler().fit(train_hit_images)
+		time_scaler = StandardScaler().fit(train_time_images)
+		self.scaler = [hit_scaler, time_scaler]
 
-		# Normalise the data
-		if self.train_data == None:	# We are looking at the largest set the training data
-			self.hit_scaler = StandardScaler().fit(hit_images)
-			self.time_scaler = StandardScaler().fit(time_images)		
+	# Decode the .txt format
+	# [Labels(1), Parameters(8), charge_pixels(32*32), time_pixels(32*32)]
+	# Parameters = [beamE, vtxX, vtxY, vtxZ, vtxT, theta, phi, lepE]
+	def decode(self, data):
+		image_data_length = self.image_size * self.image_size 	# Size of the image
 
-		hit_images_norm = self.hit_scaler.transform(hit_images)
-		time_images_norm = self.time_scaler.transform(time_images)
-			
-		#hit_images_norm  = tf.keras.utils.normalize(hit_images, axis=1)	# Norm hit images
-		#time_images_norm = tf.keras.utils.normalize(time_images, axis=1) # Norm time images
+		labels      = data[:, 0]        						# Index 0
+		parameters  = data[:, 1:9]      						# Index 1-8
+		hit_images  = data[:, 9:image_data_length+9]  			# Index 9-1032
+		time_images = data[:, image_data_length+9:]   			# Index 1033-2057
 
-		image_shape = (self.image_size, self.image_size, 1)						# Define single channel shape
-		hit_images  = hit_images.reshape(hit_images.shape[0], *image_shape)     # Reshape hit images
-		time_images = time_images.reshape(time_images.shape[0], *image_shape)   # Reshape time images
-		hit_images_norm  = hit_images_norm.reshape(hit_images_norm.shape[0], *image_shape)     # Reshape hit norm images
-		time_images_norm = time_images_norm.reshape(time_images_norm.shape[0], *image_shape)   # Reshape time norm images
-
-		# Set images depending on which channels are active
-		images = np.concatenate((hit_images, time_images), 3)    				# Combine the channels together 
-		images_norm = np.concatenate((hit_images_norm, time_images_norm), 3)    # Combine the channels together
-		if self.no_hit and not self.no_time:
-			images = time_images
-			images_norm = time_images_norm
-		elif not self.no_hit and self.no_time:
-			images = hit_images
-			images_norm = hit_images_norm
-
-		data_dict = {"labels": labels, "energies": energies, "parameters": parameters, 
-					 "images": images, "images_norm": images_norm}
-
-		if self.train_data == None:
-			self.train_data = data_dict
-		elif self.val_data == None:
-			self.val_data = data_dict
-		elif self.test_data == None:
-			self.test_data = data_dict
-
-		return
+		return labels, parameters, hit_images, time_images		# Return decoded arrays
 
 	def print(self):
 		# Training Data
-		print("Training Data -> Labels:{0} Energies:{1} Parameters:{2} Images:{3}".format(
-			self.train_data["labels"].shape, self.train_data["energies"].shape,	
-			self.train_data["parameters"].shape, self.train_data["images"].shape,	
-		))
+		print("Training Data -> Labels:{0} Parameters:{1} HitImages:{2} TimeImages:{3}".format(
+			self.data["train_labels"].shape, self.data["train_pars"].shape,	
+			self.data["train_hit_images"].shape, self.data["train_time_images"].shape))
 		# Validation Data
-		print("Validation Data -> Labels:{0} Energies:{1} Parameters:{2} Images:{3}".format(
-			self.val_data["labels"].shape, self.val_data["energies"].shape,	
-			self.val_data["parameters"].shape, self.val_data["images"].shape,	
-		))
+		print("Validation Data -> Labels:{0} Parameters:{1} HitImages:{2} TimeImages:{3}".format(
+			self.data["val_labels"].shape, self.data["val_pars"].shape,	
+			self.data["val_hit_images"].shape, self.data["val_time_images"].shape))
 		# Testing Data
-		print("Testing Data -> Labels:{0} Energies:{1} Parameters:{2} Images:{3}".format(
-			self.test_data["labels"].shape, self.test_data["energies"].shape,	
-			self.test_data["parameters"].shape, self.test_data["images"].shape,	
-		))
-		# Standard Scalers
-		#print(self.hit_scaler.mean_)
-		#print(self.time_scaler.mean_)
-		#print(self.hit_scaler.var_)
-		#print(self.time_scaler.var_)
+		print("Testing Data -> Labels:{0} Parameters:{1} HitImages:{2} TimeImages:{3}".format(
+			self.data["test_labels"].shape, self.data["test_pars"].shape,	
+			self.data["test_hit_images"].shape, self.data["test_time_images"].shape))
 
+	# Returns the shape of the images depending on which channels are active
 	def get_image_shape(self):
 		if self.no_hit or self.no_time:
 			return (self.image_size, self.image_size, 1)
 		else:
 			return (self.image_size, self.image_size, 2)
+
+	# Returns the images (0=train,1=validation,2=test)
+	def get_images(self, norm):
+		dict_keys = [("train_hit_images", "train_time_images"), 
+				("val_hit_images", "val_time_images"), 
+				("test_hit_images", "test_time_images")]
+
+		images = []	# Store the different sample images in a list
+		image_shape = (self.image_size, self.image_size, 1)	# Single channel shape
+		for sample in range(3):
+			hit_img = self.data[dict_keys[sample][0]]
+			time_img = self.data[dict_keys[sample][1]]
+			# Normalise if we need to
+			if norm == "ebe":
+				hit_img = tf.keras.utils.normalize(hit_img, axis=1)
+				time_img = tf.keras.utils.normalize(time_img, axis=1)
+			elif norm == "sss":
+				hit_img = self.scaler[0].transform(hit_img)
+				time_img = self.scaler[1].transform(time_img)
+		
+			# Reshape and combine the channels
+			hit_img = hit_img.reshape(hit_img.shape[0], *image_shape) 
+			time_img = time_img.reshape(time_img.shape[0], *image_shape) 
+			images.append(np.concatenate((hit_img, time_img), 3))
+
+		return images[0], images[1], images[2] # Train_images, Validation_images, Test_images
 
 ###################################################
 #               Callback Functions                #
@@ -277,19 +212,19 @@ def save_category_history(train_history, output_file):
 	output.close()  
 
 # Save the test output from a category based model
-def save_category_output(categories, test_labels, test_energies, test_parameters, test_output, output_file):
+def save_category_output(categories, test_labels, test_parameters, test_output, output_file):
 	print("utils: save_category_output()")
 
-	if len(test_labels) != len(test_energies) or len(test_labels) != len(test_output):
+	if len(test_labels) != len(test_parameters) or len(test_labels) != len(test_output):
 		print("Error: Arrays are not the same size")
 		sys.exit()	
 
 	# [label, beamE, parameters, testOutput[Outputs for the different categories]]
 	output = open(output_file, "w")
 	for num in range(len(test_labels)):
-		out = str(test_labels[num]) + " " + str(test_energies[num])
+		out = str(test_labels[num])
 		out += (" " + str(test_parameters[num][0]) + " " + str(test_parameters[num][1]) + " " + str(test_parameters[num][2]) + " " + str(test_parameters[num][3]))
-		out += (" " + str(test_parameters[num][4]) + " " + str(test_parameters[num][5]) + " " + str(test_parameters[num][6]))
+		out += (" " + str(test_parameters[num][4]) + " " + str(test_parameters[num][5]) + " " + str(test_parameters[num][6]) + " " + str(test_parameters[num][7]))
 		for category in range(categories):
 			out += (" " + str(test_output[num][category]))
 		out += "\n"
@@ -317,7 +252,7 @@ def save_regression_history(train_history, output_file):
 	output.close()  
 
 # Save the test output from a regression based model
-def save_regression_output(test_labels, test_energies, test_parameters, test_output, output_file):
+def save_regression_output(test_labels, test_parameters, test_output, output_file):
 	print("Output: save_regression_output")
 
 	if len(test_labels) != len(test_output):
@@ -327,9 +262,9 @@ def save_regression_output(test_labels, test_energies, test_parameters, test_out
 	# [label, beamE, parameters, test_output]
 	output = open(output_file, "w")
 	for num in range(len(test_labels)):
-		out = str(test_labels[num]) + " " + str(test_energies[num])
+		out = str(test_labels[num])
 		out += (" " + str(test_parameters[num][0]) + " " + str(test_parameters[num][1]) + " " + str(test_parameters[num][2]) + " " + str(test_parameters[num][3]))
-		out += (" " + str(test_parameters[num][4]) + " " + str(test_parameters[num][5]) + " " + str(test_parameters[num][6]))
+		out += (" " + str(test_parameters[num][4]) + " " + str(test_parameters[num][5]) + " " + str(test_parameters[num][6]) + " " + str(test_parameters[num][7]))
 		out += (" " + str(test_output[num, 0])) + "\n"
 		output.write(out)
 	output.close()     
