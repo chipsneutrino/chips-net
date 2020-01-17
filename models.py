09210a1e4d985259a0299bad6aad94d7af6ff582
jtingey <j.tingey.16@ucl.ac.uk>
2020-01-17 11:17:24 +0000

    feat: added classification network, and model selection based on config file

--- models.py
+++ models.py
@@ -19,8 +19,6 @@ class BaseModel:
     """Base model class which all implementations derive from."""
     def __init__(self, config):
         self.config = config
-        self.build()
-        self.compile()
 
     def compile(self):
         """Compiles the model."""
@@ -88,14 +86,72 @@ class SingleParModel(BaseModel):
         self.metrics = ["mae", "mse"]
         self.es_monitor = "val_mae"
 
-    def study_parameters(config):
+        self.compile()
+
+    def study_parameters(self):
         """Returns the list of possible parameters for a SHERPA study."""
         pars = [
-            sherpa.Ordinal(name='batch_size', range=config.batch_size),
-            sherpa.Continuous(name='l_rate', range=config.l_rate, scale='log'),
-            sherpa.Ordinal(name='dense_units', range=config.dense_units),
-            sherpa.Continuous(name='dropout', range=config.dropout),
-            sherpa.Ordinal(name='kernel_size', range=config.kernel_size),
-            sherpa.Ordinal(name='filters', range=config.filters),
+            sherpa.Ordinal(name='batch_size', range=self.config.batch_size),
+            sherpa.Continuous(name='l_rate', range=self.config.l_rate, scale='log'),
+            sherpa.Ordinal(name='dense_units', range=self.config.dense_units),
+            sherpa.Continuous(name='dropout', range=self.config.dropout),
+            sherpa.Ordinal(name='kernel_size', range=self.config.kernel_size),
+            sherpa.Ordinal(name='filters', range=self.config.filters),
         ]
         return pars
+
+
+class ClassificationModel(BaseModel):
+    """Event classification model class."""
+    def __init__(self, config):
+        super().__init__(config)
+
+    def build(self):
+        """Builds the model using the keras functional API."""
+        inputs = Input(shape=self.config.img_shape, name='img')
+        x = Conv2D(self.config.filters, self.config.kernel_size, padding='same', activation='relu')(inputs)
+        x = Conv2D(self.config.filters, self.config.kernel_size, activation='relu')(x)
+        x = MaxPooling2D(pool_size=2)(x)
+        x = Dropout(self.config.dropout)(x)
+        x = Conv2D((self.config.filters*2), self.config.kernel_size, padding='same', activation='relu')(x)
+        x = Conv2D((self.config.filters*2), self.config.kernel_size, activation='relu')(x)
+        x = MaxPooling2D(pool_size=2)(x)
+        x = Dropout(self.config.dropout)(x)
+        x = Conv2D((self.config.filters*4), self.config.kernel_size, padding='same', activation='relu')(x)
+        x = Conv2D((self.config.filters*4), self.config.kernel_size, activation='relu')(x)
+        x = MaxPooling2D(pool_size=2)(x)
+        x = Dropout(self.config.dropout)(x)
+        x = Flatten()(x)
+        x = Dense(self.config.dense_units, activation='relu')(x)
+        x = Dropout(self.config.dropout)(x)
+        outputs = Dense(self.conf.categories, activation='softmax', name="category")(x)
+        self.model = Model(inputs=inputs, outputs=outputs, name='classification_model')
+
+        self.loss = "sparse_categorical_crossentropy"
+        self.metrics = ["accuracy"]
+        self.es_monitor = "val_acc"
+
+        self.compile()
+
+    def study_parameters(self):
+        """Returns the list of possible parameters for a SHERPA study."""
+        pars = [
+            sherpa.Ordinal(name='batch_size', range=self.config.batch_size),
+            sherpa.Continuous(name='l_rate', range=self.config.l_rate, scale='log'),
+            sherpa.Ordinal(name='dense_units', range=self.config.dense_units),
+            sherpa.Continuous(name='dropout', range=self.config.dropout),
+            sherpa.Ordinal(name='kernel_size', range=self.config.kernel_size),
+            sherpa.Ordinal(name='filters', range=self.config.filters),
+        ]
+        return pars
+
+
+def get_model(config):
+    """Returns the correct model for the configuration."""
+    if config.model == "single_par_model":
+        return SingleParModel(config)
+    elif config.model == "classification_model":
+        return ClassificationModel(config)
+    else:
+        print("Error: model not valid!")
+        raise SystemExit
--- train.py
+++ train.py
@@ -21,7 +21,7 @@ import tensorflow as tf
 
 def run_study(config):
     """Creates a SHERPA hyperparameter study."""
-    pars = models.SingleParModel.study_parameters(config)
+    pars = models.get_model(config).study_parameters()
     algorithm = sherpa.algorithms.RandomSearch(max_num_trials=config.trials)
     study = sherpa.Study(parameters=pars, algorithm=algorithm, lower_is_better=False,
                          output_dir=config.exp_dir)
@@ -33,14 +33,23 @@ def run_study(config):
 
         train_ds, val_ds, test_ds = data.datasets(config.input_dirs, config.img_shape)
 
-        model = models.SingleParModel(config)
+        model = models.get_model(config)
+        model.build()
         cb = [study.keras_callback(trial, objective_name='val_mae')]
         model.fit(train_ds, val_ds, cb)
         study.finalize(trial)
     study.save()
 
 
-def train_model(config)
+def train_model(config):
+    """Trains a model according to the configuration."""
+    train_ds, val_ds, test_ds = data.datasets(config.input_dirs, config.img_shape)
+    model = models.get_model(config)
+    model.build()
+    train_ds.cache()
+    val_ds.cache()
+    model.fit(train_ds, val_ds)
+    evaluate_model(model, test_ds)
 
 
 def evaluate_model(model, test_ds):
@@ -54,7 +63,6 @@ def parse_args():
     """Parse the command line arguments."""
     parser = argparse.ArgumentParser(description='CHIPS CVN')
     parser.add_argument('config', help='path to the configuration file')
-    parser.add_argument('--study', action='store_true', help='Are we running a sherpa study?')
     return parser.parse_args()
 
 
@@ -63,19 +71,17 @@ def main():
     print("\nCHIPS CVN - It's Magic\n")
 
     args = parse_args()
-    config = config.process_config(args.config)
+    conf = config.process_config(args.config)
 
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
-        if args.study:
-            run_study(config)
+        if conf.type == "study":
+            run_study(conf)
+        if conf.type == "train":
+            train_model(conf)
         else:
-            train_ds, val_ds, test_ds = data.datasets(config.input_dirs, config.img_shape)
-            model = models.SingleParModel(config)
-            train_ds.cache()
-            val_ds.cache()
-            model.fit(train_ds, val_ds)
-            evaluate_model(model, test_ds)
+            print("Error: type not valid!")
+            raise SystemExit
 
 
 if __name__ == '__main__':
