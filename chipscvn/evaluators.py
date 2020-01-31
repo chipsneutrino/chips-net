08260fb5c02d873fdafc420367bbb6ab5af6ee25
jtingey <j.tingey.16@ucl.ac.uk>
2020-01-31 14:14:14 +0000

    feat: added prediction methods in evaluators to produce pandas dataframes fill with predictions and labels from a trained model on a test dataset

--- chipscvn/evaluators.py
+++ chipscvn/evaluators.py
@@ -8,6 +8,11 @@ models. All are derived from the BaseTrainer class and allow for
 customisation of the training loop if required.
 """
 
+import time
+
+import pandas as pd
+import numpy as np
+
 import chipscvn.data as data
 import chipscvn.utils as utils
 
@@ -43,3 +48,40 @@ class BasicEvaluator(BaseEvaluator):
         self.model.load()
         result = self.model.model.evaluate(self.data.test_data())
         print(result)
+
+
+def predict(data, model):
+    """Make predictions on some data given a model."""
+    start_time = time.time()  # Time how long it takes
+
+    pred_dict = {parameter: [] for parameter in model.parameters}
+    labels_dict = {
+        'pdg': [],
+        'type': [],
+        'vtxX': [],
+        'vtxY': [],
+        'vtxZ': [],
+        'dirTheta': [],
+        'dirPhi': [],
+        'nuEnergy': [],
+        'lepEnergy': []
+    }
+
+    for x, y in data:  # Run prediction on individual batches
+        predictions = model.model.predict(x)
+        for counter, key in enumerate(pred_dict.keys()):
+            if len(pred_dict) == 1:
+                pred_dict[key].extend(predictions)
+            else:
+                pred_dict[key].extend(predictions[counter])
+        for name, array in list(y.items()):
+            labels_dict[name].append(array.numpy())
+
+    for key in labels_dict:
+        labels_dict[key] = np.concatenate(labels_dict[key]).ravel()
+
+    labels = pd.DataFrame.from_dict(labels_dict)
+    predictions = pd.DataFrame.from_dict(pred_dict)
+
+    print("--- %s seconds to test model ---" % (time.time() - start_time))
+    return labels, predictions
