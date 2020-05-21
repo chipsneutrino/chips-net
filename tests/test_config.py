25171e08197611dda0bc601135aa8aed5c5331af
jtingey <j.tingey.16@ucl.ac.uk>
2020-05-21 09:30:40 +0100

    fix: test configuration path

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,13 +4,13 @@ import chipsnet.config as config
 
 
 def test_get():
-    config_path = './data/config/train.yaml'
+    config_path = './config/train.yaml'
     conf = config.get(config_path)
     assert conf.task == 'train'
 
 
 def test_setup_dirs():
-    config_path = './data/config/train.yaml'
+    config_path = './config/train.yaml'
     conf = config.get(config_path)
     config.setup_dirs(conf, False)
     assert path.exists(conf.exp.exp_dir)
